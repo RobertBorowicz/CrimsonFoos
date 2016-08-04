a6d82fa4c6cf6effa5e15e4fef23eb8803247834
Borowicz <borowicr@borowicr-01-mbr.advisory.com>
2016-08-04 11:33:02 -0500

    SQL Queries added

--- Rome-Hackathon/src/main/java/rome/database/DBConnection.java
+++ Rome-Hackathon/src/main/java/rome/database/DBConnection.java
@@ -3,6 +3,7 @@ package rome.database;
 import javax.sql.DataSource;
 import java.sql.*;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
 import rome.model.base.Player;
 import java.util.List;
 import java.util.ArrayList;
@@ -28,6 +29,81 @@ public class DBConnection {
         }
     }
 
+    public List<Player> getPlayersAsList() {
+        List<Player> players = new ArrayList<>();
+
+        try {
+            rs = st.executeQuery("SELECT player_id, first_name, last_name, nickname FROM players");
+            while (rs.next()) {
+                Player p = new Player();
+                p.setId(rs.getInt(1));
+                p.setFirstName(rs.getString(2));
+                p.setLastName(rs.getString(3));
+                p.setNickname(rs.getString(4));
+                players.add(p);
+            }
+        } catch (SQLException s) {
+            s.printStackTrace();
+        }
+
+        return players;
+
+    }
+
+    public void addNewPlayer(Player newPlayer) {
+        String insertPlayer = "INSERT INTO players (first_name, last_name, nickname)"
+                            + "VALUES (?,?,?)";
+        try {
+            PreparedStatement pst = conn.prepareStatement(insertPlayer);
+            pst.setString(1, newPlayer.getFirstName());
+            pst.setString(2, newPlayer.getLastName());
+            pst.setString(3, newPlayer.getNickname());
+        } catch (SQLException s) {
+            s.printStackTrace();
+        }
+    }
+
+    public int getTeam(int pid1, int pid2) {
+        int teamID = -1;
+
+        String getTeam = "SELECT team_id FROM team_member_bridge WHERE player_id=? "
+                       + "AND team_id IN (SELECT team_id FROM team_member_bridge WHERE player_id=?)";
+
+        try {
+            PreparedStatement pst = conn.prepareStatement(getTeam);
+            pst.setInt(1, pid1);
+            pst.setInt(2, pid2);
+            rs = pst.executeQuery();
+
+            if (rs.next()) {
+                teamID = rs.getInt("team_id");
+            }
+
+        } catch (SQLException s) {
+            s.printStackTrace();
+        }
+
+        return teamID;
+    }
+
+    public void createTeam(String nickname, int pid1, int pid2) {
+        String insertTeam = "CALL createTeam(?,?,?)";
+
+        try {
+            PreparedStatement pst = conn.prepareStatement(insertTeam);
+            pst.setString(1, nickname);
+            pst.setInt(2, pid1);
+            pst.setInt(3, pid2);
+            pst.execute();
+        } catch (SQLException s) {
+            s.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Close database connection
+     */
     public void close() {
         try {
             conn.close();
