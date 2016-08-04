b5b38b4393dcf9cda85871ac4967b293a3d870d3
Borowicz <borowicr@borowicr-01-mbr.advisory.com>
2016-08-04 15:18:34 -0500

    Staging for merge

--- Rome-Hackathon/src/test/java/rome/database/DBConnectionTest.java
+++ Rome-Hackathon/src/test/java/rome/database/DBConnectionTest.java
@@ -1,6 +1,11 @@
 package rome.database;
 
 import org.junit.Test;
+import rome.model.base.Game;
+import rome.model.base.Player;
+import rome.model.base.Stats;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -11,7 +16,19 @@ public class DBConnectionTest {
     @Test
     public void close() throws Exception {
         DBConnection db = new DBConnection();
+        List<Player> players = db.getPlayersAsList();
+        for (Player p : players) {
+            System.out.println(String.format("ID: %1$d, First: %2$s, Last: %3$s, Nickname: %4$s:", p.getId(), p.getFirstName(), p.getLastName(), p.getNickname()));
+        }
         db.close();
+    }
+
+    @Test
+    public void createTeam() throws Exception {
+        DBConnection db = new DBConnection();
+
+        Stats stats = db.getPlayerStats(1);
+        System.out.println(stats.getGamesPlayed() + " " + stats.getWins() + " " + stats.getLosses());
     }
 
 }
